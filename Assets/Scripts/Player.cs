c5ca5cfcbc61bc19ca0df2a4e659264b45fbb80f
Adalaen <hunterangelovic@gmail.com>
2019-09-30 14:43:41 -0600

    Simple player setup
    Add my own test screen.
    Add simple player setup/movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed; //The amount to multiply the player movement input by.
+    private Transform TF;//The Transform for the attached object, set at start.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TF = gameObject.GetComponent<Transform>(); //Get attached objects Transform.
+    }
+
+    //Movement should be done in FixedUpdate to look proper.
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            TF.position += new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0);
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            TF.position += new Vector3(0, 0, Input.GetAxis("Vertical") * moveSpeed);
+        }
+    }
+}
