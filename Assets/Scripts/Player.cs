2278588f813aec4446ca9b283f6123d9c7ad6603
Adalaen <hunterangelovic@gmail.com>
2019-10-02 14:36:03 -0600

    Voice Recognition v0.1
    Updated Projected to win only so that it can use the mic.
    Add Voice Recognition to the project.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+enum voiceCommand { Left, Right, Null}
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed; //The amount to multiply the player movement input by.
+    [SerializeField] private float moveMultiply; //The amount to multiply the player movement input by.
+    [SerializeField] private float moveSpeed;
     private Transform TF;//The Transform for the attached object, set at start.
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> atctions = new Dictionary<string, Action>();
+    private voiceCommand vCommand = voiceCommand.Null;
 
     // Start is called before the first frame update
     void Start()
     {
         TF = gameObject.GetComponent<Transform>(); //Get attached objects Transform.
+
+        atctions.Add("left", MoveLeft);
+        atctions.Add("right", MoveRight);
+        atctions.Add("stop", MoveStop);
+
+        keywordRecognizer = new KeywordRecognizer(atctions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedKeyword;
+        keywordRecognizer.Start();
     }
 
     //Movement should be done in FixedUpdate to look proper.
     private void FixedUpdate()
     {
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            TF.position += new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0);
-        }
-        if (Input.GetAxis("Vertical") != 0)
-        {
-            TF.position += new Vector3(0, 0, Input.GetAxis("Vertical") * moveSpeed);
-        }
+        if (vCommand == voiceCommand.Left) TF.position += new Vector3((moveSpeed * moveMultiply) * -1, 0, 0);
+        if (vCommand == voiceCommand.Right) TF.position += new Vector3(moveSpeed * moveMultiply, 0, 0);
+
+        if (Input.GetAxis("Horizontal") != 0) TF.position += new Vector3(Input.GetAxis("Horizontal") * moveMultiply, 0, 0);
+        if (Input.GetAxis("Vertical") != 0) TF.position += new Vector3(0, 0, Input.GetAxis("Vertical") * moveMultiply);
+    }
+
+    private void MoveLeft()
+    {
+        vCommand = voiceCommand.Left;
+    }
+
+    private void MoveRight()
+    {
+        vCommand = voiceCommand.Right;
+    }
+
+    private void MoveStop()
+    {
+        vCommand = voiceCommand.Null;
+    }
+
+    private void RecognizedKeyword(PhraseRecognizedEventArgs speach)
+    {
+        Debug.Log(speach.text);
+        atctions[speach.text].Invoke();
     }
 }
