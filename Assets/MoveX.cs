55cd7edc79c193bbd1680fb4797e0e527da08ad8
ChaosMartel <46508819+ChaosMartel@users.noreply.github.com>
2019-10-28 15:12:45 -0600

    Add files via upload

---
+++ Assets/MoveX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Xvalue;
+
+public class MoveX : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (StaticData.xValue != 0)
+        {
+            transform.localPosition = new Vector3(((StaticData.xValue-650) / 65), 0, 10);
+        }
+    }
+}
